4678f6516609559eca80baa3b6b6e0c4373dce69
Mike Saravayskiy <maiksaray@gmail.com>
2020-11-07 18:16:58 +0300

    [WIP] oops, forgot SessionDao for some reason

---
+++ app/dao/SessionDao.scala
@@ -0,0 +1,27 @@
+package dao
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+import dao.mapping.{Session, User}
+import javax.inject.Singleton
+
+import scala.collection.mutable
+import implicits.UserTransform._
+
+
+@Singleton
+class SessionDao {
+
+  private val sessions = mutable.Map.empty[String, Session]
+
+  def getSession(token: String): Option[Session] = sessions.get(token)
+
+  def generateToken(user:common.User):String = generateToken(user.name)
+
+  def generateToken(username: String): String = {
+    val token = s"$username-token-${UUID.randomUUID().toString}"
+    sessions.put(token, Session(token, username, LocalDateTime.now().plusHours(6)))
+    token
+  }
+}
